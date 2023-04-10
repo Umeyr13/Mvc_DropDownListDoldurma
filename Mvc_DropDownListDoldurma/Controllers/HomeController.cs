c214e3bc5365c3e8104e953d96a5551b456033df
umeyr13 <umeyr.gulcimen@gmail.com>
2023-04-10 14:37:02 +0300

    Add project files.

---
+++ Mvc_DropDownListDoldurma/Controllers/HomeController.cs
@@ -0,0 +1,65 @@
+﻿using Mvc_DropDownListDoldurma.Models;
+using Mvc_DropDownListDoldurma.Models.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mvc_DropDownListDoldurma.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            SelectList sehirler =new SelectList(Sehir.SehirGetir(), "SehirId","SehirAdi");
+
+            SelectList ulkeler = new SelectList(Ulke.UlkeGetir(), "UlkeId", "UlkeAdi");
+
+            ViewBag.sehirler = sehirler;
+            ViewBag.ulkeler = ulkeler;
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(int SehirBilgisi, int UlkeBilgisi)
+        {
+            ViewBag.UlkeId= UlkeBilgisi;
+            ViewBag.SehirId= SehirBilgisi;
+
+            SelectList sehirler = new SelectList(Sehir.SehirGetir(), "SehirId", "SehirAdi");
+
+            SelectList ulkeler = new SelectList(Ulke.UlkeGetir(), "UlkeId", "UlkeAdi");
+
+            ViewBag.sehirler = sehirler;
+            ViewBag.ulkeler = ulkeler;
+            return  View();
+        }
+
+        public ActionResult Index2()
+        {
+       
+            IndexModel model = new IndexModel()
+            {
+                 SecilenSehirId     =-1
+                ,SecilenUlkeId      =-1
+                ,UlkeListesi = new SelectList(Ulke.UlkeGetir(), "UlkeId", "UlkeAdi")
+                ,SehirListesi= new SelectList(Sehir.SehirGetir(), "SehirId", "SehirAdi")
+
+            };
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Index2(IndexModel model)
+        {
+            model.UlkeListesi= new SelectList(Ulke.UlkeGetir(),"UlkeId","UlkeAdi");
+            model.SehirListesi= new SelectList(Sehir.SehirGetir(),"SehirId,SehirAdi");
+            ViewBag.SecilenUlkeId = model.SecilenUlkeId;
+            ViewBag.SecilenSehirId = model.SecilenSehirId;
+            return View(model);
+        }
+    }
+}
---
+++ Mvc_DropDownListDoldurma/Models/ViewModel/IndexModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mvc_DropDownListDoldurma.Models.ViewModel
+{
+    public class IndexModel
+    {
+        public int SecilenSehirId { get; set; }
+        public int SecilenUlkeId { get; set; }
+        public SelectList SehirListesi { get; set; }
+        public SelectList UlkeListesi { get; set; }
+
+    }
+}
